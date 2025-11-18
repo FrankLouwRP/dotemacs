12a1a177d072bf64c08e4440c53876d63dac94fe
Frank Louw <frank.louw@redpathmining.com>
2025-11-18 15:40:55 -0500

    Simlified init.el

--- init.el
+++ init.el
@@ -1,75 +1,28 @@
-(require 'package)
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil)
- '(desktop-save-mode nil)
- '(fido-vertical-mode t)
- '(hyperbole-mode t)
- '(org-agenda-files
-   '("c:/Users/frank.louw/OneDrive - redpathmining.com/Documents/_Files/notes/notes.org"))
- '(org-capture-templates nil)
- '(org-fold-core-style 'overlays)
- '(org-list-allow-alphabetical t)
- '(org-log-into-drawer t)
- '(package-selected-packages
-   '(docker helm hyperbole magit marginalia org-bullets which-key))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(truncate-lines t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "SystemWindow" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "outline" :family "Courier New")))))
-
-(use-package docker
-  :ensure t)
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode +1))
-(use-package marginalia
-  :ensure t
-  :config
-  (marginalia-mode t))
-
-(use-package magit
-  :ensure t)
-
-(add-hook 'org-mode-hook (lambda ()
-			   (visual-line-mode)
-			   (org-indent-mode)))
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 (global-set-key [remap list-buffers] 'ibuffer)
-(setq org-directory "c:/Users/frank.louw/OneDrive - redpathmining.com/Documents/_Files/notes")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(setq org-agenda-files '("c:/Users/frank.louw/OneDrive - redpathmining.com/Documents/_Files/notes/notes.org"))
 
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline org-default-notes-file "Inbox")
-         "* NEXT %?\n  %i\n  %a")
-        ("i" "Inbox Note" entry (file+headline org-default-notes-file "Inbox")
-         "* %?\n:LOGBOOK:\n- Entered on %U\n  %i\n:END:")
-	("m" "Meeting" entry (file+headline org-default-notes-file "Meeting notes")
-         "* %? %U\n** Attendees\n- Frank Louw\n** Notes\n** Questions\n** Actions\n")
-	("j" "Journal" entry (file+datetree org-default-notes-file "Journal")
-         "* %?\n:LOGBOOK:\n- Entered on %U\n  %i\n:END:")))
-
-
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-
-(setq line-spacing 3)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(fido-vertical-mode t)
+(which-key-mode t)
 (setq visible-bell t)
+(line-number-mode t)
+(global-hl-line-mode t)
+(blink-cursor-mode -1)x
+(setq line-spacing 3)
 
-(global-hl-line-mode 1)
-(blink-cursor-mode -1)
+(keymap-global-set "C-c c" 'org-capture)
+(keymap-global-set "C-c a" 'org-agenda)
+(keymap-global-set "C-c l" 'org-store-link)
+(setq org-directory "~/gtd")
+(setq org-agenda-files (list org-directory))
+(setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+(setq org-archive-location (concat org-directory "/archive.org::"))
+
+(add-hook 'org-mode-hook (lambda ()
+			   (org-indent-mode)
+			   (visual-line-mode)))
+
+(set-face-attribute 'default nil :height 130)
